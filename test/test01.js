3491433ecbf8738d2619b2bcc69290c1e9a07e6f
ahernandezr05 <datw.alberto@gmail.com>
2021-07-28 18:39:47 +0200

    FINAL Tiempo: 10 minutos Intentos 1

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,15 @@
 function tiers(index) {
+	for ( i = 0; i < index; i++) {
+        tier = '';
+        for (j = 1; j < index - i; j++) {
+          tier = tier + ' ';
+        }
+        for ( j = 0; j < i + 1; j++) {
+          tier = tier + '*';
+        }
+        console.log(tier);
+      }
 	return "";
 }
+
 module.exports = tiers;
